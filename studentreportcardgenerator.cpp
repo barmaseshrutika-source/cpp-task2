81eb7b2df08557c8237a944d4e0a3a731dec3dd3
Shrutika Barmase <barmaseshrutika@gmail.com>
2026-02-18 14:42:40 +0530

    Add files via upload

---
+++ studentreportcardgenerator.cpp
@@ -0,0 +1,60 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+
+#include <iostream>
+using namespace std;
+
+int calculateTotal(int marks[], int n) {
+    int total = 0;
+    for(int i = 0; i < n; i++) {
+        total += marks[i];
+    }
+    return total;
+}
+
+
+char calculateGrade(float percentage) {
+    if(percentage >= 75)
+        return 'A';
+    else if(percentage >= 60)
+        return 'B';
+    else if(percentage >= 50)
+        return 'C';
+    else if(percentage >= 40)
+        return 'D';
+    else
+        return 'F';
+}
+
+int main() {
+    int n;
+
+    cout << "Enter number of subjects (3 to 5): ";
+    cin >> n;
+
+    int marks[n];
+
+    cout << "Enter marks:\n";
+    for(int i = 0; i < n; i++) {
+        cout << "Subject " << i+1 << ": ";
+        cin >> marks[i];
+    }
+
+    int total = calculateTotal(marks, n);
+    float percentage = total / (float)n;
+    char grade = calculateGrade(percentage);
+
+    cout << "\n===== Report Card =====\n";
+    cout << "Total Marks: " << total << endl;
+    cout << "Percentage: " << percentage << "%" << endl;
+    cout << "Grade: " << grade << endl;
+
+    return 0;
+}
+
